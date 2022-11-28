2c291a1d2b6465f4d64b70a588b478c5564c0865
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-11-28 21:36:30 +0300

    task5_working

--- task5_hard/Program.cs
+++ task5_hard/Program.cs
@@ -1,9 +1,16 @@
-﻿// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет, является ли оно палиндромом. Через строку нельзя решать само собой.
+﻿// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет, является ли оно палиндромом.
 Console.WriteLine("Введите число");
-int num=Convert.ToInt32(Console.ReadLine());
-int numDigit = (int) Math.Log(num,10)+1;
-Console.WriteLine("digit "+numDigit);
-string PalinCheck (int arg)
+int num = Convert.ToInt32(Console.ReadLine());
+int numDigit = (int)Math.Log(num, 10) + 1;
+int arg = numDigit / 2;
+int res = 0;
+while (arg > 0)
 {
-    
+    int div = num % 10;
+    res = res + div * (int)Math.Pow(10, arg-1);
+    num = num / 10;
+    arg--;
 }
+if(numDigit%2!=0) num = num / 10;
+if(num==res) Console.WriteLine("Число является палиндромом");
+else Console.WriteLine("Число не является палиндромом");

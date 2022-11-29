9c059742e652aeff73e357016070c0063d448db6
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-11-29 21:14:50 +0300

    task4

--- task_4_hard/Program.cs
+++ task_4_hard/Program.cs
@@ -1,14 +1,43 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в N-мерном пространстве. Сначала задается N с клавиатуры, потом задаются координаты точек.
 Console.WriteLine("Введите размерность пространства");
-int size = mod(Convert.ToInt32(Console.ReadLine()));
+int dimSpace = mod(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine("Введите координаты точки А");
+int[] arrayA = inputArray(dimSpace);
+Console.WriteLine("Введите координаты точки B");
+int[] arrayB = inputArray(dimSpace);
+Console.WriteLine("Расстояние между точками A и B: "+distCalc(arrayA, arrayB));
 
 
 
 
+
+
+double distCalc(int[] array1, int array2)
+{
+    int size = array1.Length;
+    int degSum = 0;
+    for(int i=0;i<size;i++)
+    {
+        degSum = degSum + (int)Math.Pow(array2[i]-array1[i],2);
+    }
+    return Math.Round(Math.Sqrt(degSum),2);
+}
 
 
 int mod(int num)
 {
     if (num<0) num=-num;
     return num;
+}
+
+
+int[] inputArray(int size)
+{
+    int[] array = new int [size];
+    for(int i=0; i<size;i++)
+    {
+        Console.WriteLine($"Введите {i+1} координату");
+        array[i]=Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
 }
